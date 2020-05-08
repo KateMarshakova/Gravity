9a2eca9c45b63a152a7bae8a4e29af0d7b407570
KateMarshakova <62849370+KateMarshakova@users.noreply.github.com>
2020-05-08 17:39:50 +0300

    ADD (class: works tabs)
    add tabs for work-tabs__item and made them active

--- js/main.js
+++ js/main.js
@@ -18,4 +18,11 @@ $(function () {
             }
         }
     });
+
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function(e) {
+           e.preventDefault()
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('section.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
 });
