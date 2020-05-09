d3c50bdd95d25c2da3aa4bea160af315f4e3ce71
KateMarshakova <62849370+KateMarshakova@users.noreply.github.com>
2020-05-09 13:31:47 +0300

    ADD(new selection)
    add new section "testimonials"
    p.s. to do carusel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('.owl-carousel').owlCarousel({
+    $('.works-carousel').owlCarousel({
         loop:true,
         margin:30,
         nav:true,
@@ -24,5 +24,25 @@ $(function () {
         $(this)
           .addClass('active').siblings().removeClass('active')
           .closest('section.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-      });
+    });
+
+    $('.contributor-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        dots:false,
+        navText: ['<img src="img/left.svg">','<img src="img/right.svg">'],
+        startPosition: 1,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
 });
